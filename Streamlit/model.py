72af030660fa86354cd6b24abc209488f34c723e
Abdelrhman Elruby <abdelrhmantr@gmail.com>
2023-04-22 16:37:55 +0200

    modified:   Streamlit/model.py

--- Streamlit/model.py
+++ Streamlit/model.py
@@ -8,6 +8,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.preprocessing import MinMaxScaler
 import pickle
 import streamlit as st
+import os
 
 def playlist_model(url, model, max_gen=3, same_art=5):
     log = []
@@ -16,13 +17,21 @@ def playlist_model(url, model, max_gen=3, same_art=5):
      log.append('Start logging')
      uri = url.split('/')[-1].split('?')[0]
      try:
+        log.append('spotify local method')
         stream = open("Spotify/Spotify.yaml")
         spotify_details = yaml.safe_load(stream)
         auth_manager = SpotifyClientCredentials(client_id=spotify_details['Client_id'], client_secret=spotify_details['client_secret'])
      except:
-        Client_id=st.secrets['Client ID']
-        client_secret=st.secrets['Client secret']
-        auth_manager = SpotifyClientCredentials(client_id=Client_id, client_secret=client_secret)
+        log.append('spotify .streamlit method')
+        try:
+            Client_id=st.secrets["Client_ID"]
+            client_secret=st.secrets["Client_secret"]
+            auth_manager = SpotifyClientCredentials(client_id=Client_id, client_secret=client_secret)
+        except:
+            log.append('spotify hug method')
+            Client_id=os.environ['Client_ID']
+            client_secret=os.environ['Client_secret']
+            auth_manager = SpotifyClientCredentials(client_id=Client_id, client_secret=client_secret)
      sp = spotipy.client.Spotify(auth_manager=auth_manager) 
 
      if model == 'Spotify Model':
